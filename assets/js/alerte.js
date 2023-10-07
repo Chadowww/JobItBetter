2d748b4fa023cf062bff3881f7dd767722710f96
chado <a.sale@outlook.fr>
2023-10-07 08:51:04 +0200

    Feat: add management of the red icon alert

--- assets/js/alerte.js
+++ assets/js/alerte.js
@@ -1,3 +1,18 @@
+function notifManagment()
+{
+    let iconNotification = document.getElementById('icon-notification');
+    let iconNotificationBtn = document.getElementById('icon-notification-button');
+    let bodyAlerts = document.querySelector('.offcanvas-body');
+    let linkAlerts = bodyAlerts.querySelectorAll('.fw-bold.link-alert');
+    if (linkAlerts.length >= 1) {
+        iconNotification.classList.remove('d-none');
+        iconNotificationBtn.classList.remove('d-none');
+    } else {
+        iconNotification.classList.add('d-none');
+        iconNotificationBtn.classList.add('d-none');
+    }
+}
+
 function readAlert(event)
 {
     event.preventDefault();
@@ -19,6 +34,7 @@ function readAlert(event)
                     alertIcon.classList.remove("fw-light");
                     alertIcon.classList.add("fw-bold");
                 }
+                notifManagment();
             });
     } catch (err) {
         console.error(err);
@@ -47,3 +63,4 @@ function deleteAlert(event)
 }
 
 window.deleteAlert = deleteAlert;
+notifManagment();
