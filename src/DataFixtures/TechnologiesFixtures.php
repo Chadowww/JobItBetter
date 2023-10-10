24f0fe313057830f120aa0a8f2ea77537853a428
chado <a.sale@outlook.fr>
2023-10-10 12:10:13 +0200

    Feat: Update Fixtures

--- src/DataFixtures/TechnologiesFixtures.php
+++ src/DataFixtures/TechnologiesFixtures.php
@@ -4,30 +4,118 @@ namespace App\DataFixtures;
 
 use App\Entity\Technology;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class TechnologiesFixtures extends Fixture
+class TechnologiesFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        $technologies = [
-            "HTML", "CSS", "JavaScript", "jQuery", "Bootstrap", "Angular", "React", "Vue.js", "PHP",
-            "Laravel", "Symfony", "Python", "Django", "Ruby", "Ruby on Rails", "Node.js", "Express.js", "Java",
-            "Spring Framework", ".NET", "C#", "ASP.NET", "Python", "Flask", "Ruby", "Ruby on Rails", "ASP.NET",
-            "C#", ".NET Core", "Tailwind", "Laravel", "CodeIgniter", "WordPress", "Drupal", "Joomla", "Magento",
-            "MySQL", "PostgreSQL", "MongoDB", "Oracle", "SQL Server", "Git", "Docker", "Kubernetes",
-            "AWS (Amazon Web Services)", "Azure", "Google Cloud", "Heroku", "RESTful API", "GraphQL"
+        $frontEndTechnologies = [
+            "HTML", "CSS", "JavaScript", "jQuery", "Bootstrap", "Angular", "React", "Vue.js"
         ];
-        $i = 0;
+        $backEndTechnologies = [
+            "PHP", "Laravel", "Symfony", "Python", "Django", "Ruby", "Ruby on Rails", "Node.js", "Express.js"
+        ];
+        $aplicationTechnologies = [
+            "Java", "Spring Framework", ".NET", "C#", "ASP.NET", ".NET Core", "C++", "Go", "Rust"
+        ];
+        $otherWebTechnologies = [
+             "Tailwind", "CodeIgniter", "WordPress", "Drupal", "Joomla", "Magento"
+        ];
+        $databaseTechnologies = [
+            "MySQL", "PostgreSQL", "MongoDB", "Oracle", "SQL Server"
+        ];
+        $versionControlTechnologies = [
+            "Git", "Docker", "Kubernetes"
+        ];
+        $cloudTechnologies = [
+            "AWS (Amazon Web Services)", "Azure", "Google Cloud", "Heroku"
+        ];
+        $apiTechnologies = [
+            "RESTful API", "GraphQL"
+        ];
 
-        foreach ($technologies as $technology) {
+        $j = 1;
+        foreach ($frontEndTechnologies as $technology) {
             $techno = new Technology();
             $techno->setName($technology);
-            $manager->persist((new Technology())->setName($technology));
-            $this->addReference('technology_' . $i, $techno);
-            $i++;
+            $techno->setCategory($this->getReference('category_1'));
+            $this->addReference('technology_' . $j, $techno);
+            $manager->persist($techno);
+            $j++;
         }
+        $j = 9;
 
+        foreach ($backEndTechnologies as $technology) {
+            $techno = new Technology();
+            $techno->setName($technology);
+            $techno->setCategory($this->getReference('category_2'));
+            $this->addReference('technology_' . $j, $techno);
+            $manager->persist($techno);
+            $j++;
+        }
+        $j = 18;
+        foreach ($otherWebTechnologies as $technology) {
+            $techno = new Technology();
+            $techno->setName($technology);
+            $techno->setCategory($this->getReference('category_4'));
+            $this->addReference('technology_' . $j, $techno);
+            $manager->persist($techno);
+            $j++;
+        }
+        $j = 24;
+        foreach ($databaseTechnologies as $technology) {
+            $techno = new Technology();
+            $techno->setName($technology);
+            $techno->setCategory($this->getReference('category_4'));
+            $this->addReference('technology_' . $j, $techno);
+            $manager->persist($techno);
+            $j++;
+        }
+        $j = 29;
+        foreach ($versionControlTechnologies as $technology) {
+            $techno = new Technology();
+            $techno->setName($technology);
+            $techno->setCategory($this->getReference('category_5'));
+            $this->addReference('technology_' . $j, $techno);
+            $manager->persist($techno);
+            $j++;
+        }
+        $j = 32;
+        foreach ($cloudTechnologies as $technology) {
+            $techno = new Technology();
+            $techno->setName($technology);
+            $techno->setCategory($this->getReference('category_6'));
+            $this->addReference('technology_' . $j, $techno);
+            $manager->persist($techno);
+            $j++;
+        }
+        $j = 36;
+        foreach ($apiTechnologies as $technology) {
+            $techno = new Technology();
+            $techno->setName($technology);
+            $techno->setCategory($this->getReference('category_7'));
+            $this->addReference('technology_' . $j, $techno);
+            $manager->persist($techno);
+            $j++;
+        }
+        $j = 38;
+        foreach ($aplicationTechnologies as $technology) {
+            $techno = new Technology();
+            $techno->setName($technology);
+            $techno->setCategory($this->getReference('category_3'));
+            $this->addReference('technology_' . $j, $techno);
+            $manager->persist($techno);
+            $j++;
+        }
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            CategoryFixtures::class,
+        ];
     }
 }
