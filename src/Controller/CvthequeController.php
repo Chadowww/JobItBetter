a448678a12eafaecc85244967df9ff14cdf1a46b
chado <a.sale@outlook.fr>
2023-10-12 10:37:04 +0200

    Feat: Add logic for filtrer CV by technologies

--- src/Controller/CvthequeController.php
+++ src/Controller/CvthequeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use AllowDynamicProperties;
+use App\Form\CvthequeType;
 use App\Repository\ResumeRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,6 +21,23 @@ use Symfony\Component\Routing\Annotation\Route;
     #[Route('/cvtheque', name: 'app_cvtheque')]
     public function index(Request $request): Response
     {
+        $form = $this->createForm(CvthequeType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $resumes = $this->resumeRepository->searchResumes($form->getData());
+            $resumes = $this->paginator->paginate(
+                $resumes,
+                $request->query->getInt('page', 1),
+                10
+            );
+            return $this->render('cvtheque/index.html.twig', [
+                'resumes' => $resumes,
+                'form' => $form->createView(),
+            ]);
+        }
+
+
         $resumes = $this->resumeRepository->findAll();
         $resumes = $this->paginator->paginate(
             $resumes,
@@ -28,6 +46,7 @@ use Symfony\Component\Routing\Annotation\Route;
         );
         return $this->render('cvtheque/index.html.twig', [
             'resumes' => $resumes,
+            'form' => $form->createView(),
         ]);
     }
 }
