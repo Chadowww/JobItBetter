f64ef659b7f697d106213f8a0ea6d2c5b50a9691
chado <a.sale@outlook.fr>
2023-10-20 10:42:19 +0200

    Feat: Add new method for create alert about Company

--- src/Services/AlertService.php
+++ src/Services/AlertService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Entity\Alert;
+use App\Entity\Company;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -23,6 +24,16 @@ class AlertService
         $this->alert->setApplicant($resumeUser);
         $this->alert->setEmployer($user->getCompany());
         $this->alert->setMessage('a  vu votre candidature.');
+        $this->alert->setState(true);
+        $this->entityManager->persist($this->alert);
+        $this->entityManager->flush();
+    }
+
+    public function addAlertCompany(UserInterface $user, Company $company): void
+    {
+        $this->alert->setApplicant($user);
+        $this->alert->setEmployer($company);
+        $this->alert->setMessage('vous avez un nouveau candidat?');
         $this->alert->setState(true);
         $this->entityManager->persist($this->alert);
         $this->entityManager->flush();
