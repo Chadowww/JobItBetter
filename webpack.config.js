024d9007c364d191835203fdf803339a3d7d7d7c
chado <a.sale@outlook.fr>
2023-10-24 17:45:38 +0200

    Feat: ADD config for preset React

--- webpack.config.js
+++ webpack.config.js
@@ -71,6 +71,7 @@ Encore
 
     .configureBabel((config) => {
         config.plugins.push('@babel/plugin-proposal-class-properties');
+        config.presets.push('@babel/preset-react'); // Ajoutez cette ligne
     })
 
     // enables @babel/preset-env polyfills
