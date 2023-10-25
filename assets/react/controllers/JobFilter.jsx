7652c995a8d1c0a8d1e2d3361280c762058023af
chado <a.sale@outlook.fr>
2023-10-25 08:30:35 +0200

    Feat: Update for prop types & add button

--- assets/react/controllers/JobFilter.jsx
+++ assets/react/controllers/JobFilter.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 function App({categories, technologies }) {
 
     return (
         <div className="form-accordion-parent row d-flex justify-content-around">
-            <form action="" className="form-accordion accordion p-0 position-relative col-3" id="accordionExample">
+            <form className="form-accordion accordion p-0 position-relative col-3" id="accordionExample">
                 <div className="be-joboffer-card-template  sticky-element ">
                     <h2 className=" py-4 fs-5 m-0 w-100 d-flex justify-content-center bg-white be-h2 ">Affinez votre <span>&nbsp recherche</span></h2>
                     {
@@ -17,24 +18,24 @@ function App({categories, technologies }) {
                                             type="button"
                                             data-bs-toggle="collapse"
                                             data-bs-target={'#collapse' + category.id}
-                                            aria-expanded="true"
+                                            aria-expanded="false"
                                             aria-controls={'collapse' + category.id}
                                         >{category.name}</button>
                                     </h2>
                                     <div
-                                        className="accordion-collapse collapse show"
+                                        className="accordion-collapse collapse"
                                         id= {'collapse' + category.id}
                                     >
                                         <div className="accordion-body d-flex flex-wrap justify-content-between p-0">
                                             {technologies.map((technology) => {
                                                 return (
                                                     category.name === technology.category ? (
-                                                        <div className="checkbox d-flex justify-content-between col-6 p-2" key={technology.id}>
+                                                        <div className="checkbox d-flex justify-content-between col-5 p-2" key={technology.id}>
+                                                            <input id={'technology' + technology.id} type="checkbox" value={technology.id} />
                                                             <label htmlFor={'technology' + technology.id} className={'d-flex' +
-                                                                ' justify-content-evenly'}>
+                                                                ' justify-content-evenly fw-bolder'}>
                                                                 {technology.name}
                                                             </label>
-                                                            <input id={'technology' + technology.id} type="checkbox" value={technology.id} />
                                                         </div>
                                                     ) : null
                                                 )
@@ -45,10 +46,17 @@ function App({categories, technologies }) {
                             )
                         })
                     }
+                    <div className={'w-100 d-flex justify-content-center'}>
+                        <button type="submit" className={'be-primary-button m-3'}>Filtrer</button>
+                    </div>
                 </div>
             </form>
         </div>
     )
 }
 
+App.proptypes = {
+    categories: PropTypes.array.isRequired,
+    technologies: PropTypes.array.isRequired,
+}
 export default App;
